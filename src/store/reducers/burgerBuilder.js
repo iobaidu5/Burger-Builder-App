6ecd56edbc332bd4652d12b0d383660bd143dfcd
iobaidu5 <i.obaidu5@gmail.com>
2021-01-23 16:31:34 +0500

    Redux Advanced Added

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,9 +1,9 @@
 import * as actionType from '../actions/actionTypes';
-
+import { updateObject } from '../utility';
 
 const INGREDIENT_PRICE = {
     salad: 0.5,
-    cheese: 0.4,
+    cheese: 0.4, 
     meat: 1.3, 
     bacon: 0.7
 };
